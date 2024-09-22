8bf7e1674d34de8c2fc01da05133e060c34945be
tebioinformatics <te.bioinformatics@gmail.com>
2024-09-22 13:13:34 +0900

    Update DESeq2 script

--- scripts/DESeq2_gene_RE-_vs_Control.R
+++ scripts/DESeq2_gene_RE-_vs_Control.R
@@ -1,5 +1,7 @@
+# 2024/08/29
 # Differential gene expression testing from expression data using DESeq2
 # Remove all previous data
+
 
 #if (!requireNamespace("BiocManager", quietly = TRUE))
 #install.packages("BiocManager")
@@ -13,36 +15,39 @@
 #biocLite("DESeq2")
 #library("DESeq2")
 
+
 #source ("http://bioconductor.org/biocLite.R")
 
 #BiocManager::install("tximport")
 
 rm(list=ls())
-###
+##############################################################################################
 
-path <- "path/to/your/directory"
+path <- "path/to/your/working/directory"
 setwd(path)
 
 library(DESeq2)
 library(tximport)
 
 # Load the list containing sample information
-s2c <- read.table("Import_Sample_List_Case_vs_Control.txt", header=T, sep="\t", stringsAsFactors=F)
+sample_info_file <- "Sample_List_YYYYMMDD.txt"
+s2c <- read.table(sample_info_file, header=T, sep="\t", stringsAsFactors=F)
 files <- s2c$path
 names(files) <- s2c$sample
 
 # Load expression data (analysis at gene-level)
 txi <- tximport(files, type="rsem", txIn=F, txOut=F)
 dim(as.data.frame(txi))
-write.table(txi, file="DataMatrix_ImportSampleList_Case_vs_Control.txt", sep="\t", quote=F)
+output_matrix_file <- "DataMatrix_YYYYMMDD.txt"
+write.table(txi, file=output_matrix_file, sep="\t", quote=F)
 
 # Replace length=0 with 1
 txi$length[txi$length==0] <- 1
 sampleTable <- data.frame(condition=s2c$group,RIN=s2c$RIN)
 rownames(sampleTable) <- colnames(txi$counts)
-
 # Explicitly specify factor type
-sampleTable$condition <- as.factor(sampleTable$condition)　# Explicitly specify factor type
+sampleTable$condition <- as.factor(sampleTable$condition) # Explicitly specify factor type
+
 
 dds <- DESeqDataSetFromTximport(txi, sampleTable,
                                 design = ~ RIN + condition)
@@ -51,7 +56,7 @@ dds <- DESeqDataSetFromTximport(txi, sampleTable,
 
 # Wald test (2-group comparison)
 dds_wt <- DESeq(dds)
-res_wt <- results(dds_wt, contrast=c("condition", "case","control")) 　# Specify the order of subtraction for log2FoldChange
+res_wt <- results(dds_wt, contrast=c("condition", "nex","control")) # Specify the order of subtraction for log2FoldChange
 dim(res_wt)
 
 summary(res_wt)
@@ -60,4 +65,5 @@ res_wt_naomit <- na.omit(res_wt) # Remove missing values
 dim(res_wt_naomit)
 
 # Write out data
-write.table(res_wt_naomit, file="DESeq2_Result_of_WaldTest_Case_vs_Control.txt", sep="\t", quote=F)
+output_file <- "DESeq2_Result_YYYYMMDD.txt"
+write.table(res_wt_naomit, file=output_file, sep="\t", quote=F)
